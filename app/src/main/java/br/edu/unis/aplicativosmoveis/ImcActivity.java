0036d2e1933c3e55729162675206685847b44ca6
Angelo Mesquita <angelo.mesquita@professor.unis.edu.br>
2022-03-22 20:58:33 -0300

    Exibe activity com a lista de apps criados

---
+++ app/src/main/java/br/edu/unis/aplicativosmoveis/ImcActivity.java
@@ -0,0 +1,15 @@
+package br.edu.unis.aplicativosmoveis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class ImcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
