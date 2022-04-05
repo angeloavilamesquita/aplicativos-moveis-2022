b049861a5131bde750b81c744cd7827e8cb61744
Angelo Mesquita <angelo.mesquita@professor.unis.edu.br>
2022-04-05 11:09:45 -0300

    inclui app imc e app gasolina

--- app/src/main/java/br/edu/unis/aplicativosmoveis/FramelayoutActivity.java
+++ app/src/main/java/br/edu/unis/aplicativosmoveis/FramelayoutActivity.java
@@ -3,13 +3,12 @@ package br.edu.unis.aplicativosmoveis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
-public class ImcActivity extends AppCompatActivity {
+public class FramelayoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_imc);
+        setContentView(R.layout.activity_framelayout);
     }
 }
